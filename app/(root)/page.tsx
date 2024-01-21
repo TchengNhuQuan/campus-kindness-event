09149cea6231259c7499c2fcf38baf9b32724784
Nhu Quan <nhuquan@amsvn.com>
2024-01-21 19:55:59 +0700

    commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
+
+      </section>
+    </>
+  )
+}
