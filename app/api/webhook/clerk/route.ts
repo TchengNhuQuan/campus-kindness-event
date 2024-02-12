45c28ceb2ffe99c11f9ad1b4be831e5b88be0b8a
Nhu Quan <tcheng.nhuquan@gmail.com>
2024-02-12 16:55:29 +0700

    refactor

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      clerkId: "user_2cGGXPyLS52KhH5GZKlxHC15efc",
+      clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
