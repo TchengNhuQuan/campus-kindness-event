6492808a46d963ba088374978e3c974d6ee0f149
Nhu Quan <tcheng.nhuquan@gmail.com>
2024-02-17 11:24:00 +0700

    fix: deploy to vercel fail

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -4,9 +4,7 @@ import { Button } from "../ui/button";
 import { IEvent } from "@/lib/database/models/event.model";
 import { checkoutOrder } from "@/lib/actions/order.actions";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   useEffect(() => {
